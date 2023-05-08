0d4298d6f93d324bbd7ac83f36a25919d69e743f
ouriel tapiero <oht862@gmail.com>
2023-05-08 12:29:45 +0300

    changed the doc format

--- pptx_parser/pptx_reader.py
+++ pptx_parser/pptx_reader.py
@@ -1,16 +1,16 @@
-'''
-this files contains function to read a PowerPoint file
-'''
+"""
+ this file is used to read a pptx file and extract its slides.
+"""
+
 import pptx
 
 def read_pptx_file(path: str)->list[pptx.slide.Slide]:
-    '''
-    read a PowerPoint file and return a list of slides
-    :param
-        path: str, path to the file
-    :return:
-        list of slides
-    '''
+    """
+       Read a PowerPoint file and extract its slides.
+
+       :param path: The path to the PowerPoint file.
+       :return: A list of slides in the PowerPoint file.
+    """
     pptx_file = pptx.Presentation(path)
     # Extract the slides from the file
     slides = []
--- pptx_parser/slide_text_extractor.py
+++ pptx_parser/slide_text_extractor.py
@@ -1,20 +1,19 @@
-'''
- this module is used to extract text from pptx file
-'''
+"""
+ this file is used to extract text from pptx file
+"""
 import pptx
 
-import pptx_reader
-import text_processing
+from pptx_parser import pptx_reader
+from pptx_parser import text_processing
 
 
-def get_pptx_file_content ( pptx_file_path: str ) -> list[list[str]]:
-    '''
-    extract text from a pptx file
-    :param
-        pptx_file: pptx file object
-    :return:
-        list of lists, each list contains title, text and notes of a slide
-    '''
+def get_pptx_file_content( pptx_file_path: str ) -> list[list[str]]:
+    """
+    extract text from a pptx file.
+
+    :param pptx_file_path: str, path to the pptx file.
+    :return list of lists, each list contains title, text and notes of a slide.
+    """
     pptx_file_slides = pptx_reader.read_pptx_file(pptx_file_path)
     pptx_file_content = []
     for slide in pptx_file_slides:
@@ -26,14 +25,14 @@ def get_pptx_file_content ( pptx_file_path: str ) -> list[list[str]]:
     return pptx_file_content
 
 
+
 def get_slide_text ( slide: pptx.slide.Slide ) -> str:
-    '''
-    extract text from a slide
-    :param
-        slide: pptx.slide.Slide object
-    :return:
-        str, text from the slide
-    '''
+    """
+    extract text from a slide.
+
+    :param slide: pptx.slide.Slide object.
+    :return: str, text from the slide.
+    """
     text = ""
     for shape in slide.shapes:
         if hasattr(shape, "text"):
@@ -42,13 +41,12 @@ def get_slide_text ( slide: pptx.slide.Slide ) -> str:
 
 
 def get_slide_title ( slide: pptx.slide.Slide ) -> str:
-    '''
-    extract title from a slide
-    :param
-        slide: pptx.slide.Slide object
-    :return:
-        str, title from the slide
-    '''
+    """
+    extract title from a slide.
+
+    :param slide: pptx.slide.Slide object.
+    :return: str, title from the slide
+    """
     title = slide.shapes.title
     if title is not None:
         return title.text
@@ -56,13 +54,12 @@ def get_slide_title ( slide: pptx.slide.Slide ) -> str:
 
 
 def get_slide_notes ( slide: pptx.slide.Slide ) -> str:
-    '''
-    extract notes from a slide
-    :param
-        slide: pptx.slide.Slide object
-    :return:
-        str, notes from the slide
-    '''
+    """
+    extract notes from a slide.
+
+    :param slide: pptx.slide.Slide object.
+    :return: str, notes from the slide.
+    """
     notes = ""
     for shape in slide.notes_slide.shapes:
         if hasattr(shape, "text"):
